90efa068a0f34862e783b4a42f4fa06758474991
chrissmuel40-web <chrissmuel40@gmail.com>
2026-09-02 10:19:28 +0700

    Create menu.php

---
+++ models/menu.php
@@ -0,0 +1,36 @@
+    <?php
+
+$products = [
+    [
+        "name" => "Adidas Samba",
+        "price" => "Rp 1.800.000",
+        "image" => "images/adidas.jpeg"
+    ],
+    [
+        "name" => "Converse Classic",
+        "price" => "Rp 1.200.000",
+        "image" => "images/converse.jpeg"
+    ],
+    [
+        "name" => "New Balance 550",
+        "price" => "Rp 2.300.000",
+        "image" => "images/new ballance.jpeg"
+    ],
+    [
+        "name" => "Jordan 1 Retro",
+        "price" => "Rp 3.500.000",
+        "image" => "images/airjordan.jpeg"
+    ],
+    [
+        "name" => "Puma RS-X",
+        "price" => "Rp 1.500.000",
+        "image" => "images/puma.jpeg"
+    ],
+    [
+        "name" => "Vans Old Skool",
+        "price" => "Rp 950.000",
+        "image" => "images/vans.jpeg"
+    ]
+];
+?>
+    
