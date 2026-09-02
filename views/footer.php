378b771a9f25c9a5831be2624c8eb182496155e1
chrissmuel40-web <chrissmuel40@gmail.com>
2026-09-02 10:20:23 +0700

    Add footer with social media links and copyright

---
+++ views/footer.php
@@ -0,0 +1,13 @@
+    <footer id="contact" class="main-footer">
+    <div style="text-align:center; padding: 80px 5%; border-top: 1px solid #222;">
+        <h4 style="letter-spacing: 3px; color: #444; margin-bottom: 20px;">STAY CONNECTED</h4>
+        <div class="social-icons">
+            <a href="#" style="color: #fff; margin: 0 15px; font-size: 1.5rem;"><i class="fab fa-instagram"></i></a>
+            <a href="#" style="color: #fff; margin: 0 15px; font-size: 1.5rem;"><i class="fab fa-tiktok"></i></a>
+        </div>
+        <p style="margin-top: 30px; color: #333; font-size: 0.8rem;">© 2026 LIANEL KICKS. BONDOWOSO PRIDE.</p>
+    </div>
+</footer>
+</body>
+</html>
+    
